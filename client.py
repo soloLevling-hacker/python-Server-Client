c56ff733dbdbe78a767431e21f2d922ad018f780
soloLevling-hacker <khushrupapara5@gmail.com>
2025-09-26 15:32:56 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+
+
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.1.36"
+ADDR = (SERVER, PORT)
+
+client =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+print("Type your message and press Enter.")
+print(f"To exit, type {DISCONNECT_MESSAGE}\n")
+
+if __name__ == "__main__":
+    while True:
+        msg = input("> ")
+        send(msg)
+        if msg == DISCONNECT_MESSAGE:
+            break
+        
